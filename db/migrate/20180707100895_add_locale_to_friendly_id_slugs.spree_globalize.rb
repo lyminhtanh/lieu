ac9b112ab96375dc7086b91f9294c3a63c8149b2
Unknown <lyminhtanh@gmail.com>
2018-08-10 17:27:35 +0700

    hard remove migration related to globalize gem

--- db/migrate/20180707100895_add_locale_to_friendly_id_slugs.spree_globalize.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from spree_globalize (originally 20170713120642)
-class AddLocaleToFriendlyIdSlugs < SpreeExtension::Migration[4.2]
-  def change
-    add_column :friendly_id_slugs, :locale, :string
-  end
-end
--- db/migrate/20180707100897_change_translation_meta_title_to_product.spree_globalize.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from spree_globalize (originally 20171005151819)
-class ChangeTranslationMetaTitleToProduct < SpreeExtension::Migration[4.2]
-  def change
-    change_column :spree_product_translations, :meta_title, :string
-  end
-end
